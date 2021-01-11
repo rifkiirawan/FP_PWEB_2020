0a2bcc4c9ddaff3d2bc3fff0678630831c11ec0f
Adrian Danindra <60964359+Bluejake3@users.noreply.github.com>
2021-01-11 21:49:34 +0700

    tambah password

--- tugas3.sql
+++ tugas3.sql
@@ -1,31 +1,12 @@
 /*==============================================================*/
 /* DBMS name:      MySQL 5.0                                    */
-/* Created on:     06/01/2021 11:41:49                          */
+/* Created on:     11/01/2021 21:31:23                          */
 /*==============================================================*/
 
 
-alter table PEMINJAMAN 
-   drop foreign key FK_PEMINJAM_BUKU_DIPI_BUKU;
-
-alter table PEMINJAMAN 
-   drop foreign key FK_PEMINJAM_PELAYAN_P_PETUGAS;
-
-alter table PEMINJAMAN 
-   drop foreign key FK_PEMINJAM_PEMINJAM_TAMU;
+drop table if exists PEMINJAMAN;
 
 drop table if exists BUKU;
-
-
-alter table PEMINJAMAN 
-   drop foreign key FK_PEMINJAM_BUKU_DIPI_BUKU;
-
-alter table PEMINJAMAN 
-   drop foreign key FK_PEMINJAM_PEMINJAM_TAMU;
-
-alter table PEMINJAMAN 
-   drop foreign key FK_PEMINJAM_PELAYAN_P_PETUGAS;
-
-drop table if exists PEMINJAMAN;
 
 drop table if exists PETUGAS;
 
@@ -48,9 +29,9 @@ create table BUKU
 create table PEMINJAMAN
 (
    P_ID                 int not null  comment '',
-   PT_ID                int not null  comment '',
    B_ID                 int not null  comment '',
-   T_ID                 int not null  comment '',
+   T_USERNAME           varchar(20) not null  comment '',
+   PT_ID                int not null  comment '',
    P_MULAI              timestamp  comment '',
    P_SELESAI            date  comment '',
    ATTRIBUTE_11         varchar(10)  comment '',
@@ -63,7 +44,8 @@ create table PEMINJAMAN
 create table PETUGAS
 (
    PT_ID                int not null  comment '',
-   PT_NAMA              varchar(20)  comment '',
+   PT_NAMA              varchar(50)  comment '',
+   PT_JK                char(1)  comment '',
    primary key (PT_ID)
 );
 
@@ -72,21 +54,22 @@ create table PETUGAS
 /*==============================================================*/
 create table TAMU
 (
-   T_ID                 int not null  comment '',
+   T_USERNAME           varchar(20) not null  comment '',
    T_NAMA               varchar(50)  comment '',
    T_ALAMAT             varchar(100)  comment '',
    T_PEKERJAAN          varchar(20)  comment '',
    T_TELEPON            varchar(15)  comment '',
    T_IMAGEPATH          varchar(100)  comment '',
-   primary key (T_ID)
+   T_PASSWORD           varchar(20)  comment '',
+   primary key (T_USERNAME)
 );
 
-alter table PEMINJAMAN add constraint FK_PEMINJAM_BUKU_DIPI_BUKU foreign key (B_ID)
+alter table PEMINJAMAN add constraint FK_PEMINJAM_PEMINJAM_TAMU foreign key (T_USERNAME)
+      references TAMU (T_USERNAME) on delete restrict on update restrict;
+
+alter table PEMINJAMAN add constraint FK_PEMINJAM_RELATIONS_BUKU foreign key (B_ID)
       references BUKU (B_ID) on delete restrict on update restrict;
 
-alter table PEMINJAMAN add constraint FK_PEMINJAM_PELAYAN_P_PETUGAS foreign key (PT_ID)
+alter table PEMINJAMAN add constraint FK_PEMINJAM_RELATIONS_PETUGAS foreign key (PT_ID)
       references PETUGAS (PT_ID) on delete restrict on update restrict;
-
-alter table PEMINJAMAN add constraint FK_PEMINJAM_PEMINJAM_TAMU foreign key (T_ID)
-      references TAMU (T_ID) on delete restrict on update restrict;
 
